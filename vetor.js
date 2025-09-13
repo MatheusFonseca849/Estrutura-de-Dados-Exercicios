9e0d365c1f253da63ce5ae61ce81a25fb7da88ca
MatheusFonseca849 <matheusfonseca849@gmail.com>
2025-09-13 15:02:21 -0300

    exercício finalizado

--- vetor.js
+++ vetor.js
@@ -14,7 +14,8 @@ class Vector {
 
     list() {
         if (this.#length === 0) {
-            throw new Error("Lista vazia")
+            process.stdout.write("Lista vazia" + '\n')
+            return
         }
         for (let i = 0; i < this.#length; i++) {
             process.stdout.write(`${this.#data[i]}`);
@@ -71,26 +72,107 @@ class Vector {
     update(a, b){
 
         if(!this.#data.includes(a)){
-            process.stdout.write("Dado não encontrado")
-            process.stdout.write("\n")
+            process.stdout.write(`Dado ${a} não encontrado` + '\n')
             return
         }
 
         for(let i = 0 ; i < this.#length ; i++){
             if (this.#data[i] == a){
                 this.#data[i] = b
+                break
+            }
+        }
+    }
+
+    updateAll(a, b){
+        for(let i = 0 ; i < this.#length ; i++){
+            if (this.#data[i] == a){
+                this.#data[i] = b
+            }
+        }
+    }
+
+    updateByIndex(index, element){
+        if (index < 0 || index >= this.#length) {
+            process.stdout.write("Índice inválido\n");
+            return;
+        }
+        this.#data[index] = element;
+    }
+
+    search(element){
+        for(let i = 0 ; i < this.#length ; i++){
+            if (this.#data[i] == element){
+                process.stdout.write(`Elemento ${element} encontrado no índice ${i}` + '\n')
+                return i
+            }
+        }
+        process.stdout.write(`Elemento ${element} não encontrado` + '\n')
+        return -1
+    }
+
+    searchByIndex(index){
+        if (index < 0 || index >= this.#length) {
+            process.stdout.write("Índice inválido\n");
+            return;
+        }
+        process.stdout.write(`Elemento ${this.#data[index]} encontrado no índice ${index}` + '\n')
+        return this.#data[index]
+    }
+
+    remove(element){
+        if (!this.#data.includes(element)) {
+            process.stdout.write(`Elemento ${element} não encontrado` + '\n')
+            return
+        }
+        for (let i = 0; i < this.#length; i++) {
+            if (this.#data[i] == element) {
+                this.#data[i] = this.#data[i + 1]
+            }
+        }
+        this.#length--
+    }
+
+    removeByIndex(index){
+        if (index < 0 || index >= this.#length) {
+            process.stdout.write("Índice inválido\n");
+            return;
+        }
+        
+        for (let i = index; i < this.#length; i++) {
+            this.#data[i] = this.#data[i + 1]
+        }
+        this.#length--
+    }
+
+    sort(){
+        for (let i = 0; i < this.#length; i++) {
+            for (let j = i + 1; j < this.#length; j++) {
+                if (this.#data[i] > this.#data[j]) {
+                    let aux = this.#data[i]
+                    this.#data[i] = this.#data[j]
+                    this.#data[j] = aux
+                }
             }
         }
     }
 }
 
 let v = new Vector(10)
-v.insertOrdered(99)
-v.insertOrdered(25)
-v.insertOrdered(10)
-v.insertOrdered(5)
-v.insertOrdered(12)
-v.insertOrdered(22)
-v.insertOrdered(55)
-v.update(101,0)
+v.insert(99)
+v.insert(25)
+v.insert(99)
+v.insert(10)
+v.insert(5)
+v.insert(12)
+v.insert(99)
+v.insert(22)
+v.insert(55)
+v.updateAll(99,101)
+v.searchByIndex(5)
+v.search(101)
+v.remove(101)
+v.removeByIndex(5)
+v.list()
+v.sort()
 v.list()
